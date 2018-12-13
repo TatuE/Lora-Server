1b77856b5ed49a34bb8d5ba3ef9a576b2f110e59
TatuE <tatu.erkinjuntti@gmail.com>
2018-12-14 00:50:38 +0200

    update

--- src/servlet/Servlet_AddSensorData.java
+++ src/servlet/Servlet_AddSensorData.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import dao.Dao_Device;
 import dao.Dao_DevicePlacement;
 import dao.Dao_SensorData;
+import helper.ValueChecker;
 import model.DevicePlacement;
 
 @WebServlet("/Servlet_AddSensorData")
@@ -26,11 +27,12 @@ public class Servlet_AddSensorData extends HttpServlet {
 		Dao_Device daoD = new Dao_Device();
 		Dao_DevicePlacement daoDp = new Dao_DevicePlacement();
 		Dao_SensorData daoSd = new Dao_SensorData();
+		ValueChecker vc = new ValueChecker();
 		
 		try {
 			String uid = request.getParameter("loraUid");
 			int data = Integer.parseInt(request.getParameter("sensorValue"));
-			if(!uid.contains("<")||!uid.contains("'")||uid.length()<=10) {
+			if(!vc.ifValid(uid)||uid.length()<=10) {
 				if(data==0||data==1) {
 					DevicePlacement devicePlacement = daoDp.getDevicePlacement("device_id", daoD.getDevice(uid));				
 					if(devicePlacement.getDevice_placement_id()>0) {
